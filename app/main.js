266339ecab349112cddf684762a9631124b30650
Chris Browder <rcbrowder@gmail.com>
2018-05-30 16:36:22 -0400

    Adds onreadystatechange, getWeather function, and httpRequestOnError function.

--- app/main.js
+++ app/main.js
@@ -16,3 +16,42 @@ var imag = document.getElementById('imag');
 // API stuff
 var apiRequest;
 var appId = "7dc8be0140f5ffb29046006b555a09c8";
+
+
+// Wait for data to load before executing any javascript
+document.onreadystatechange = function() {
+	if (document.readyState == "interactive") {
+		weatherButton.onclick = getWeather;
+	}
+};
+
+
+// Function to get info from api
+function getWeather() {
+
+	// Insert user data and appId into URL
+	var url = "http://api.openweathermap.org/data/2.5/weather?zip=<zipCode>&us&appid=<appId>";
+	url = url.replace("<zipCode>", zipInput.value);
+	url = url.replace("<appId>", appId);
+
+	// Code that fetches data from the API URL and stores it in results.
+	apiRequest = new XMLHttpRequest();
+	apiRequest.onload = catchResponse;
+	apiRequest.onerror = httpRequestOnError;
+	apiRequest.open('get', url, true);
+	apiRequest.send();
+}
+
+// Function to display error message
+function httpRequestOnError() {
+    output.style.display = 'none';
+    errorMessage.innerHTML = 'tHeRe WaS a PrObLeM ReAcHiNg ThE sErVeR. tRy AgAiN lAtEr.';
+    error.style.display = 'block';
+}
+
+// Function to disply data if getWeather is successful
+function catchResponse() {
+    if (apiRequest.statusText == 'OK') {
+        
+    }
+}
